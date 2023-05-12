fecd7ba7ce491635031c3b0fe80b03d9e503f10d
Javier Damiani <116226692+javierdamiani@users.noreply.github.com>
2023-05-12 18:34:46 -0500

    1er avance

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import logo from '../images/__logo.svg'
+
+function Header(){
+    return(
+        <>
+        <header className="header">
+            <img src={logo} className="header__logo" alt="logo" />
+            <div className="header__buttons">
+            <button className="header__button">About me</button>
+            <button className="header__button">Projects</button>
+            <button className="header__button">Contact</button>
+            </div>
+        </header>
+        </>
+    )
+}
+
+export default Header
