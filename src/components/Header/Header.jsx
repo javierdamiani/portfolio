3d55ef86c780de371e9c85da9bdb37589bb95511
Javier Damiani <javierdamiani74@gmail.com>
2023-09-29 20:20:05 -0500

    header btns ok

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,13 +2,40 @@ import React from 'react';
 import './Header.css';
 
 function Header() {
+  function scrollToAbout() {
+    window.scrollTo({
+      top: 30,
+      behavior: 'smooth',
+    });
+  }
+
+  function scrollToProjects() {
+    window.scrollTo({
+      top: 600,
+      behavior: 'smooth',
+    });
+  }
+
+  function scrollToContact() {
+    window.scrollTo({
+      top: 3000,
+      behavior: 'smooth',
+    });
+  }
+
   return (
     <>
       <header className='header'>
         <div className='header__buttons'>
-          <button className='header__button'>About me</button>
-          <button className='header__button'>Projects</button>
-          <button className='header__button'>Contact</button>
+          <button className='header__button' onClick={scrollToAbout}>
+            About me
+          </button>
+          <button className='header__button' onClick={scrollToProjects}>
+            Projects
+          </button>
+          <button className='header__button' onClick={scrollToContact}>
+            Contact
+          </button>
         </div>
       </header>
     </>
